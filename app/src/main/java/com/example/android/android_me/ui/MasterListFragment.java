95aa42c503cda69d75420cd0bcf82bc75b40bb2e
Ehbraheem <ehbraheem@gmail.com>
2017-06-03 09:31:05 -0700

    Half way through

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,25 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by prof.BOLA on 6/3/2017.
+ */
+
+public class MasterListFragment extends Fragment {
+
+    public MasterListFragment(){
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate();
+    }
+}
